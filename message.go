4f0a18238bdc9e29a11936b28af9689c91e2a587
Matt Layher <mdlayher@gmail.com>
2021-01-30 12:18:27 -0500

    ospf3: implement DatabaseDescription and LSAHeader types
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- message.go
+++ message.go
@@ -49,7 +49,7 @@ const (
 
 // String returns the string representation of an Options bitmask.
 func (o Options) String() string {
-	names := []string{
+	return flagsString(uint(o), []string{
 		"V6-bit",
 		"E-bit",
 		"x-bit",
@@ -61,34 +61,7 @@ func (o Options) String() string {
 		"AF-bit",
 		"L-bit",
 		"AT-bit",
-	}
-
-	var s string
-	left := uint(o)
-	for i, name := range names {
-		if o&(1<<uint(i)) != 0 {
-			if s != "" {
-				s += "|"
-			}
-
-			s += name
-
-			left ^= (1 << uint(i))
-		}
-	}
-
-	if s == "" && left == 0 {
-		s = "0"
-	}
-
-	if left > 0 {
-		if s != "" {
-			s += "|"
-		}
-		s += fmt.Sprintf("%#x", left)
-	}
-
-	return s
+	})
 }
 
 // headerLen is the length of an OSPFv3 header.
@@ -109,7 +82,7 @@ type Header struct {
 // marshal packs a Header's bytes into b while also setting packet type and
 // length. It assumes b has allocated enough space for a Header to avoid a
 // panic.
-func (h *Header) marshal(b []byte, ptyp packetType, plen uint16) error {
+func (h *Header) marshal(b []byte, ptyp packetType, plen uint16) {
 	b[0] = version
 	b[1] = byte(ptyp)
 	binary.BigEndian.PutUint16(b[2:4], plen)
@@ -118,8 +91,6 @@ func (h *Header) marshal(b []byte, ptyp packetType, plen uint16) error {
 	binary.BigEndian.PutUint16(b[12:14], h.Checksum)
 	b[14] = h.InstanceID
 	// b[15] is reserved.
-
-	return nil
 }
 
 // parseHeader parses an OSPFv3 Header and the offset of the end of an OSPF
@@ -191,6 +162,8 @@ func ParseMessage(b []byte) (Message, error) {
 	switch ptyp {
 	case hello:
 		m = &Hello{Header: h}
+	case databaseDescription:
+		m = &DatabaseDescription{Header: h}
 	default:
 		// TODO(mdlayher): implement more Messages!
 		return nil, fmt.Errorf("ospf3: parsing not implemented message type: %d", ptyp)
@@ -238,9 +211,7 @@ func (h *Hello) len() int {
 func (h *Hello) marshal(b []byte) error {
 	// Marshal the Header and then store the Hello bytes following it.
 	const n = headerLen
-	if err := h.Header.marshal(b[:n], hello, uint16(h.len())); err != nil {
-		return err
-	}
+	h.Header.marshal(b[:n], hello, uint16(h.len()))
 
 	binary.BigEndian.PutUint32(b[n:n+4], h.InterfaceID)
 	// Router priority is 8 bits, Options is 24 bits immediately following.
@@ -291,4 +262,221 @@ func (h *Hello) unmarshal(b []byte) error {
 	}
 
 	return nil
+}
+
+// DDFlags are flags which may appear in an OSPFv3 Database Description message
+// as described in RFC5340, appendix A.3.3.
+type DDFlags uint16
+
+// Possible DDFlags values.
+const (
+	MSBit DDFlags = 1 << 0
+	MBit  DDFlags = 1 << 1
+	IBit  DDFlags = 1 << 2
+)
+
+// String returns the string representation of a DDFlags bitmask.
+func (f DDFlags) String() string {
+	return flagsString(uint(f), []string{
+		"MS-bit",
+		"M-bit",
+		"I-bit",
+	})
+}
+
+var _ Message = &DatabaseDescription{}
+
+// A DatabaseDescription is an OSPFv3 Database Description message as described
+// in RFC5340, appendix A.3.3.
+type DatabaseDescription struct {
+	Header         Header
+	Options        Options
+	InterfaceMTU   uint16
+	Flags          DDFlags
+	SequenceNumber uint32
+	LSAs           []LSAHeader
+}
+
+// ddLen is the length of a Database Description message with no trailing array
+// of LSA headers.
+const ddLen = 12
+
+// len implements Message.
+func (dd *DatabaseDescription) len() int {
+	// Fixed Header and DatabaseDescription, plus 20 bytes per LSA header.
+	return headerLen + ddLen + (lsaHeaderLen * len(dd.LSAs))
+}
+
+// marshal implements Message.
+func (dd *DatabaseDescription) marshal(b []byte) error {
+	// Marshal the Header and then store the Database Description bytes following it.
+	const n = headerLen
+	dd.Header.marshal(b[:n], databaseDescription, uint16(dd.len()))
+
+	binary.BigEndian.PutUint32(b[n:n+4], uint32(dd.Options))
+	binary.BigEndian.PutUint16(b[n+4:n+6], dd.InterfaceMTU)
+	// b[6] is reserved.
+	b[n+7] = byte(dd.Flags)
+	binary.BigEndian.PutUint32(b[n+8:n+12], dd.SequenceNumber)
+
+	// Each LSA header is packed into 20 adjacent bytes.
+	nn := n + 12
+	for i := range dd.LSAs {
+		dd.LSAs[i].marshal(b[nn : nn+lsaHeaderLen])
+		nn += lsaHeaderLen
+	}
+
+	return nil
+}
+
+// unmarshal implements Message.
+func (dd *DatabaseDescription) unmarshal(b []byte) error {
+	if l := len(b); l < ddLen {
+		return fmt.Errorf("ospf3: not enough bytes for DatabaseDescription: %d", l)
+	}
+
+	// b[0] is reserved.
+	// Options is 24 bits.
+	dd.Options = Options(binary.BigEndian.Uint32(b[0:4]) & 0x00ffffff)
+	dd.InterfaceMTU = binary.BigEndian.Uint16(b[4:6])
+	// b[6] is reserved
+	dd.Flags = DDFlags(b[7])
+	dd.SequenceNumber = binary.BigEndian.Uint32(b[8:12])
+
+	// DatabaseDescription must end on a 20 byte boundary so we can parse any
+	// possible LSAHeaders in the trailing array.
+	const lsaOff = 12
+	if l := len(b[lsaOff:]); l%lsaHeaderLen != 0 {
+		return fmt.Errorf("ospf3: DatabaseDescription message must end on a 20 byte boundary for trailing LSA headers, got %d bytes", l)
+	}
+
+	// We now know the number of LSA headers because they have a fixed size.
+	n := len(b[lsaOff:]) / lsaHeaderLen
+	dd.LSAs = make([]LSAHeader, 0, n)
+	for i := 0; i < n; i++ {
+		// Parse each 20 byte LSA header from the slice.
+		var (
+			start = lsaOff + (i * lsaHeaderLen)
+			end   = lsaOff + lsaHeaderLen + (i * lsaHeaderLen)
+		)
+
+		dd.LSAs = append(dd.LSAs, parseLSAHeader(b[start:end]))
+	}
+
+	return nil
+}
+
+// An LSType is the type of an OSPFv3 Link State Advertisement as described in
+// RFC5340, appendix A.4.2.1.
+type LSType uint16
+
+// Possible LSType values.
+const (
+	RouterLSA          LSType = 0x2001
+	NetworkLSA         LSType = 0x2002
+	InterAreaPrefixLSA LSType = 0x2003
+	InterAreaRouterLSA LSType = 0x2004
+	ASExternalLSA      LSType = 0x4005
+	deprecatedLSA      LSType = 0x2006
+	NSSALSA            LSType = 0x2007
+	LinkLSA            LSType = 0x0008
+	IntraAreaPrefixLSA LSType = 0x2009
+)
+
+// LSAHandling returns the value of the U-bit in the LSType. False indicates the
+// LSA should be treated as if it had link-local flooding scope. True indicates
+// that a router should store and flood the LSA as if the type is understood.
+func (t LSType) LSAHandling() bool {
+	return (t & 0xf000) != 0
+}
+
+// FloodingScope returns the LSA flooding scope value stored in the S1 and S2
+// bits in the LSType.
+func (t LSType) FloodingScope() FloodingScope {
+	return FloodingScope((t & 0x6000) >> 13)
+}
+
+// A FloodingScope is an OSPFv3 LSA flooding scope as described in RFC 5340,
+// appendix A.4.2.1.
+type FloodingScope uint8
+
+// Possible FloodingScope values.
+const (
+	LinkLocalScoping FloodingScope = 0
+	AreaScoping      FloodingScope = 1
+	ASScoping        FloodingScope = 2
+	reservedScoping  FloodingScope = 3
+)
+
+// An LSAHeader is an OSPFv3 Link State Advertisement header as described in
+// RFC5340, appendix A.4.2.
+type LSAHeader struct {
+	Age               time.Duration
+	Type              LSType
+	LinkStateID       ID
+	AdvertisingRouter ID
+	SequenceNumber    uint32
+	Checksum          uint16
+	Length            uint16
+}
+
+// lsaHeaderLen is the length of an LSA header.
+const lsaHeaderLen = 20
+
+// marshal stores the LSAHeader bytes into b. It assumes b has allocated enough
+// space for an LSAHeader to avoid a panic.
+func (h LSAHeader) marshal(b []byte) {
+	binary.BigEndian.PutUint16(b[0:2], uint16(h.Age.Seconds()))
+	binary.BigEndian.PutUint16(b[2:4], uint16(h.Type))
+	copy(b[4:8], h.LinkStateID[:])
+	copy(b[8:12], h.AdvertisingRouter[:])
+	binary.BigEndian.PutUint32(b[12:16], h.SequenceNumber)
+	binary.BigEndian.PutUint16(b[16:18], h.Checksum)
+	binary.BigEndian.PutUint16(b[18:20], h.Length)
+}
+
+// parseLSAHeader unpacks an LSAHeader from a byte slice.
+func parseLSAHeader(b []byte) LSAHeader {
+	h := LSAHeader{
+		Age:            time.Duration(binary.BigEndian.Uint16(b[0:2])) * time.Second,
+		Type:           LSType(binary.BigEndian.Uint16(b[2:4])),
+		SequenceNumber: binary.BigEndian.Uint32(b[12:16]),
+		Checksum:       binary.BigEndian.Uint16(b[16:18]),
+		Length:         binary.BigEndian.Uint16(b[18:20]),
+	}
+	copy(h.LinkStateID[:], b[4:8])
+	copy(h.AdvertisingRouter[:], b[8:12])
+
+	return h
+}
+
+// flagsString generates a pretty-printed flags bitmask using the input value
+// and sequence of names.
+func flagsString(f uint, names []string) string {
+	var s string
+	left := f
+	for i, name := range names {
+		if f&(1<<uint(i)) != 0 {
+			if s != "" {
+				s += "|"
+			}
+
+			s += name
+
+			left ^= (1 << uint(i))
+		}
+	}
+
+	if s == "" && left == 0 {
+		s = "0"
+	}
+
+	if left > 0 {
+		if s != "" {
+			s += "|"
+		}
+		s += fmt.Sprintf("%#x", left)
+	}
+
+	return s
 }
--- message_test.go
+++ message_test.go
@@ -8,6 +8,81 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+var (
+	msgHello = []byte{
+		// Header
+		version,      // OSPFv3
+		uint8(hello), // Hello
+		0x00, 44,     // PacketLength
+		192, 0, 2, 1, // Router ID
+		0, 0, 0, 0, // Area ID
+		0x00, 0x00, // Checksum
+		0x01, // InstanceID
+		0x00, // Reserved
+
+		// Hello
+		0x00, 0x00, 0x00, 0x01, // Interface ID
+		0x01,                                 // Router priority
+		0x00, 0x00, byte(V6Bit) | byte(EBit), // Options
+		0x00, 0x05, // Hello interval
+		0x00, 0x0a, // Router dead interval
+		192, 0, 2, 1, // Designated router ID
+		192, 0, 2, 2, // Backup designated router ID
+		// Neighbor IDs
+		192, 0, 2, 2,
+		192, 0, 2, 3,
+
+		// LSA headers
+		0x00, 0x01,
+		0x00, 0x00,
+
+		// Trailing bytes, ignored
+		0x00, 0x00, 0x00, 0x00,
+	}
+
+	msgDatabaseDescription = []byte{
+		// Header
+		version,                    // OSPFv3
+		uint8(databaseDescription), // Hello
+		0x00, 68,                   // PacketLength
+		192, 0, 2, 1, // Router ID
+		0, 0, 0, 0, // Area ID
+		0x00, 0x00, // Checksum
+		0x01, // InstanceID
+		0x00, // Reserved
+
+		// DatabaseDescription
+		0x00, 0x00, byte(AFBit - 255), byte(V6Bit) | byte(EBit) | byte(RBit), // Options
+		0x05, 0xdc, // Interface MTU
+		0x00,                    // Reserved
+		byte(IBit) | byte(MBit), // Flags
+		0x00, 0x00, 0x00, 0x01,  // Sequence number
+
+		// LSA headers
+		//
+		// Router-LSA
+		0x00, 0x01, // Age
+		byte(RouterLSA >> 8), byte(RouterLSA & 0x00ff), // Type
+		0, 0, 0, 0, // Link state ID
+		192, 0, 2, 1, // Advertising router
+		0x00, 0x00, 0x00, 0xff, // Sequence number
+		0x00, 0x00, // Checksum
+		0x00, lsaHeaderLen, // Length
+
+		// Link-LSA
+		0x00, 0x02, // Age
+		byte(LinkLSA >> 8), byte(LinkLSA & 0x00ff), // Type
+		0, 0, 0, 5, // Link state ID
+		192, 0, 2, 1, // Advertising router
+		0x00, 0x00, 0x01, 0xff, // Sequence number
+		0x00, 0x00, // Checksum
+		0x00, lsaHeaderLen, // Length
+
+		// Trailing bytes, ignored
+		0x00, 0x00, 0x00, 0x00,
+	}
+)
+
 func TestParseMessageErrors(t *testing.T) {
 	tests := []struct {
 		name string
@@ -67,6 +142,7 @@ func TestParseMessageErrors(t *testing.T) {
 				0, 0, 0, 0,
 				0x01,
 				0x00,
+
 				0xff, // Truncated Hello
 			},
 		},
@@ -95,13 +171,55 @@ func TestParseMessageErrors(t *testing.T) {
 				192, 0, 2,
 			},
 		},
+		{
+			name: "short database description",
+			b: []byte{
+				version,
+				uint8(databaseDescription),
+				0x00, 17, // Header + 1 trailing byte
+				0x00, 0x00,
+				192, 0, 2, 1,
+				0, 0, 0, 0,
+				0x01,
+				0x00,
+
+				0xff, // Truncated Database Description
+			},
+		},
+		{
+			name: "bad database description LSAs",
+			b: []byte{
+				// Header
+				version,                    // OSPFv3
+				uint8(databaseDescription), // Hello
+				0x00, 29,                   // PacketLength
+				192, 0, 2, 1, // Router ID
+				0, 0, 0, 0, // Area ID
+				0x00, 0x00, // Checksum
+				0x01, // InstanceID
+				0x00, // Reserved
+
+				// DatabaseDescription
+				0x00, 0x00, byte(AFBit - 255), byte(V6Bit) | byte(EBit) | byte(RBit), // Options
+				0x05, 0xdc, // Interface MTU
+				0x00,                    // Reserved
+				byte(IBit) | byte(MBit), // Flags
+				0x00, 0x00, 0x00, 0x01,  // Sequence number
+
+				// LSA headers, truncated
+				0xff,
+			},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if _, err := ParseMessage(tt.b); err == nil {
+			_, err := ParseMessage(tt.b)
+			if err == nil {
 				t.Fatal("expected an error, but none occurred")
 			}
+
+			t.Logf("err: %v", err)
 		})
 	}
 }
@@ -114,32 +232,7 @@ func TestMessageRoundTrip(t *testing.T) {
 	}{
 		{
 			name: "hello",
-			b: []byte{
-				// Header
-				version,      // OSPFv3
-				uint8(hello), // Hello
-				0x00, 44,     // PacketLength
-				192, 0, 2, 1, // Router ID
-				0, 0, 0, 0, // Area ID
-				0x00, 0x00, // Checksum
-				0x01, // InstanceID
-				0x00, // Reserved
-
-				// Hello
-				0x00, 0x00, 0x00, 0x01, // Interface ID
-				0x01,                                 // Router priority
-				0x00, 0x00, byte(V6Bit) | byte(EBit), // Options
-				0x00, 0x05, // Hello interval
-				0x00, 0x0a, // Router dead interval
-				192, 0, 2, 1, // Designated router ID
-				192, 0, 2, 2, // Backup designated router ID
-				// Neighbor IDs
-				192, 0, 2, 2,
-				192, 0, 2, 3,
-
-				// Trailing bytes, ignored
-				0x00, 0x00, 0x00, 0x00,
-			},
+			b:    msgHello,
 			m: &Hello{
 				Header: Header{
 					RouterID:   ID{192, 0, 2, 1},
@@ -155,6 +248,37 @@ func TestMessageRoundTrip(t *testing.T) {
 				NeighborIDs: []ID{
 					{192, 0, 2, 2},
 					{192, 0, 2, 3},
+				},
+			},
+		},
+		{
+			name: "database description",
+			b:    msgDatabaseDescription,
+			m: &DatabaseDescription{
+				Header: Header{
+					RouterID:   ID{192, 0, 2, 1},
+					InstanceID: 1,
+				},
+				Options:        V6Bit | EBit | RBit | AFBit,
+				InterfaceMTU:   1500,
+				Flags:          IBit | MBit,
+				SequenceNumber: 1,
+				LSAs: []LSAHeader{
+					{
+						Age:               1 * time.Second,
+						Type:              RouterLSA,
+						AdvertisingRouter: ID{192, 0, 2, 1},
+						SequenceNumber:    255,
+						Length:            20,
+					},
+					{
+						Age:               2 * time.Second,
+						Type:              LinkLSA,
+						LinkStateID:       ID{0, 0, 0, 5},
+						AdvertisingRouter: ID{192, 0, 2, 1},
+						SequenceNumber:    511,
+						Length:            20,
+					},
 				},
 			},
 		},
@@ -238,32 +362,7 @@ func BenchmarkParseMessage(b *testing.B) {
 	}{
 		{
 			name: "Hello",
-			b: []byte{
-				// Header
-				version,      // OSPFv3
-				uint8(hello), // Hello
-				0x00, 44,     // PacketLength
-				192, 0, 2, 1, // Router ID
-				0, 0, 0, 0, // Area ID
-				0x00, 0x00, // Checksum
-				0x01, // InstanceID
-				0x00, // Reserved
-
-				// Hello
-				0x00, 0x00, 0x00, 0x01, // Interface ID
-				0x01,                                 // Router priority
-				0x00, 0x00, byte(V6Bit) | byte(EBit), // Options
-				0x00, 0x05, // Hello interval
-				0x00, 0x0a, // Router dead interval
-				192, 0, 2, 1, // Designated router ID
-				192, 0, 2, 2, // Backup designated router ID
-				// Neighbor IDs
-				192, 0, 2, 2,
-				192, 0, 2, 3,
-
-				// Trailing bytes, ignored
-				0x00, 0x00, 0x00, 0x00,
-			},
+			b:    msgHello,
 		},
 	}
 
